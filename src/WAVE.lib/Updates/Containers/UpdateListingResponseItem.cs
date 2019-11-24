e95912a6ff6eef13232eb6cfad5365230d1e1bd2
Jarred Capellman <jcapellman@hotmail.com>
2019-11-24 21:14:58 +0100

    Added: Linux Library project

--- src/WAVE.lib/Updates/Containers/UpdateListingResponseItem.cs
+++ src/WAVE.lib/Updates/Containers/UpdateListingResponseItem.cs
@@ -2,7 +2,7 @@
 {
     public class UpdateListingResponseItem
     {
-        public string Description;
+        public string Description { get; set; }
 
         public string Name { get; set; }
 
