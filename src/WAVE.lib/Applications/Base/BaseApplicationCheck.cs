3370e30f3002bd1c9480bb506aceb530c03db229
Jarred Capellman <jcapellman@hotmail.com>
2019-11-26 09:19:53 +0100

    Added: Initial work on the BaseApplicationCheck class

---
+++ src/WAVE.lib/Applications/Base/BaseApplicationCheck.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace WAVE.lib.Applications.Base
+{
+    public abstract class BaseApplicationCheck
+    {
+        public abstract List<string> GetInstalledApplicationsNameOnly();
+    }
+}
