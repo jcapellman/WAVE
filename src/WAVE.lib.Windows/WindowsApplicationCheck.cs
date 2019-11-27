d3379a82e74a2a8f9c503e407a9caa0cdddf28eb
Jarred Capellman <jcapellman@hotmail.com>
2019-11-27 22:50:04 +0100

    Added: WindowsApplicationCheck class

---
+++ src/WAVE.lib.Windows/WindowsApplicationCheck.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+using WAVE.lib.Applications.Base;
+
+namespace WAVE.lib.Windows
+{
+    public class WindowsApplicationCheck : BaseApplicationCheck
+    {
+        public override List<string> GetInstalledApplicationsNameOnly()
+        {
+            // TODO Implement
+            return new List<string>();
+        }
+    }
+}
