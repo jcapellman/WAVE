e6eb6831e68beb3a82e5d59440421433d61e6dc6
Jarred Capellman <jcapellman@hotmail.com>
2021-05-01 11:58:05 -0500

    Added: ApplicationResponseItem Class

---
+++ src/WAVE.lib/Applications/Containers/ApplicationResponseItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WAVE.lib.Applications.Containers
+{
+    public class ApplicationResponseItem
+    {
+        public string Name { get; set; }
+
+        public string Version { get; set; }
+
+        public string Vendor { get; set; }
+
+        public DateTime? InstallDate { get; set; }
+
+        public string InstallLocation { get; set; }
+    }
+}
