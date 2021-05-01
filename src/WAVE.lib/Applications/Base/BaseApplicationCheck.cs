3e2442926d86ade4efde4e5f6abe303f0a0ab49b
Jarred Capellman <jcapellman@hotmail.com>
2021-05-01 11:53:52 -0500

    Change: Replaced the simple String name for the full information

--- src/WAVE.lib/Applications/Base/BaseApplicationCheck.cs
+++ src/WAVE.lib/Applications/Base/BaseApplicationCheck.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
 
+using WAVE.lib.Applications.Containers;
+
 namespace WAVE.lib.Applications.Base
 {
     public abstract class BaseApplicationCheck
     {
-        public abstract List<string> GetInstalledApplicationsNameOnly();
+        public abstract List<ApplicationResponseItem> GetInstalledApplications();
     }
 }
