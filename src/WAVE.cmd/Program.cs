cf741f4df7d8d35effb94479aeb12e83ffd32909
Jarred Capellman <jcapellman@hotmail.com>
2021-05-01 11:57:51 -0500

    Added: CMD Sample App

---
+++ src/WAVE.cmd/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text.Json;
+
+using WAVE.lib.Windows;
+
+namespace WAVE.cmd
+{
+    class Program
+    {
+        [SupportedOSPlatform("windows")]
+        static void Main(string[] args)
+        {
+            var apps = new WindowsApplicationCheck().GetInstalledApplications();
+
+            File.WriteAllText("inventory.json", string.Join(Environment.NewLine, apps.Select(a => JsonSerializer.Serialize(a))));
+
+            foreach (var app in apps)
+            {
+                Console.WriteLine(JsonSerializer.Serialize(app));
+            }
+        }
+    }
+}
