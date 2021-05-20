009d1bcf9f98ad84dc7da15ae066e4c77f497604
Jarred Capellman <jcapellman@hotmail.com>
2021-05-20 18:04:42 -0500

    Added: ILogger wrappers

--- src/WAVE.lib/Applications/Base/BaseApplicationCheck.cs
+++ src/WAVE.lib/Applications/Base/BaseApplicationCheck.cs
@@ -1,11 +1,30 @@
 ﻿using System.Collections.Generic;
 
+using Microsoft.Extensions.Logging;
+
 using WAVE.lib.Applications.Containers;
 
 namespace WAVE.lib.Applications.Base
 {
     public abstract class BaseApplicationCheck
     {
+        protected ILogger _logger;
+
+        protected BaseApplicationCheck(ILogger logger = null)
+        {
+            _logger = logger;
+        }
+
+        protected void LogError(string message)
+        {
+            _logger?.LogError(message);
+        }
+
+        protected void LogDebug(string message)
+        {
+            _logger?.LogDebug(message);
+        }
+
         public abstract List<ApplicationResponseItem> GetInstalledApplications();
     }
 }
