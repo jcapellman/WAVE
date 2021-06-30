ffd978c87c7d20bd896fc2b6f146ec5a80804b0b
Jarred Capellman <jcapellman@hotmail.com>
2021-06-29 19:35:25 -0500

    Added: WAVELayer Class to wrap querying

---
+++ src/WAVE.lib/WAVELayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Extensions.Logging;
+
+using WAVE.lib.Applications.Containers;
+using WAVE.lib.Windows;
+
+namespace WAVE.lib
+{
+    public static class WAVELayer
+    {
+        public static List<ApplicationResponseItem> GetApplications(ILogger logger = null)
+        {
+            if (OperatingSystem.IsWindows())
+            {
+                return new WindowsApplicationCheck(logger).GetInstalledApplications();
+            }
+
+            // TODO: Write macOS and Linux Implementations
+            return new List<ApplicationResponseItem>();
+        }
+    }
+}
