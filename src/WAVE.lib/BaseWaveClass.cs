c2b3614a31bf6806f75a6461108c3c1ccb1cecf4
Jarred Capellman <jcapellman@hotmail.com>
2021-07-05 12:27:35 -0500

    Added: BaseWaveClass class

---
+++ src/WAVE.lib/BaseWaveClass.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace WAVE.lib
+{
+    internal class BaseWaveClass
+    {
+        protected ILogger _logger;
+
+        public BaseWaveClass(ILogger logger = null)
+        {
+            _logger = logger;
+        }
+
+        protected void LogError(string message)
+        {
+            _logger?.LogError(message);
+        }
+
+        protected void LogDebug(string message)
+        {
+            _logger?.LogDebug(message);
+        }
+    }
+}
