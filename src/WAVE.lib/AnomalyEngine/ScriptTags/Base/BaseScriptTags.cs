7d063ddb1e006633ede7f824ffa9aa50f9228e7f
Jarred Capellman <jcapellman@hotmail.com>
2021-07-15 19:19:03 -0500

    Added: Initial work on the BaseScriptTags Abstract Class

---
+++ src/WAVE.lib/AnomalyEngine/ScriptTags/Base/BaseScriptTags.cs
@@ -0,0 +1,14 @@
+﻿namespace WAVE.lib.AnomalyEngine.ScriptTags.Base
+{
+    public abstract class BaseScriptTags
+    {
+        private readonly string[] _arguments;
+
+        protected BaseScriptTags(string[] arguments)
+        {
+            _arguments = arguments;
+        }
+
+        public abstract string Run();
+    }
+}
