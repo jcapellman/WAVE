cb6093834343a2821c01292440cfa2be23430cf0
Jarred Capellman <jcapellman@hotmail.com>
2021-07-15 19:19:45 -0500

    Added: Initial work on the CheckServiceTag

---
+++ src/WAVE.lib/AnomalyEngine/ScriptTags/CheckServiceTag.cs
@@ -0,0 +1,16 @@
+﻿using WAVE.lib.AnomalyEngine.ScriptTags.Base;
+
+namespace WAVE.lib.AnomalyEngine.ScriptTags
+{
+    public class CheckServiceTag : BaseScriptTags
+    {
+        public CheckServiceTag(string[] arguments) : base(arguments)
+        {
+        }
+
+        public override string Run()
+        {
+            
+        }
+    }
+}
