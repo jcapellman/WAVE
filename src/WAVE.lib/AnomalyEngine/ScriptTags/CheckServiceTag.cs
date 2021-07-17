ee0f01f184b3610be728ef985a0894d54e710f58
Jarred Capellman <jcapellman@hotmail.com>
2021-07-16 19:53:32 -0500

    Change: Updated the CheckServiceTag class to implement the new changes in the Base class

--- src/WAVE.lib/AnomalyEngine/ScriptTags/CheckServiceTag.cs
+++ src/WAVE.lib/AnomalyEngine/ScriptTags/CheckServiceTag.cs
@@ -4,13 +4,11 @@ namespace WAVE.lib.AnomalyEngine.ScriptTags
 {
     public class CheckServiceTag : BaseScriptTags
     {
-        public CheckServiceTag(string[] arguments) : base(arguments)
-        {
-        }
+        protected override string[] ValidArguments => new[] { "servicename" };
 
-        public override string Run()
+        public override string Run(string[] arguments)
         {
-            
+            throw new System.NotImplementedException();
         }
     }
 }
