002cf9531b1809769b1657aaa2efda509f00bddc
Jarred Capellman <jcapellman@hotmail.com>
2021-07-17 20:06:53 -0500

    Change: Flushed out the CheckServiceTag

--- src/WAVE.lib/AnomalyEngine/ScriptTags/CheckServiceTag.cs
+++ src/WAVE.lib/AnomalyEngine/ScriptTags/CheckServiceTag.cs
@@ -1,14 +1,33 @@
-﻿using WAVE.lib.AnomalyEngine.ScriptTags.Base;
+﻿using System;
+using System.Runtime.Versioning;
+using System.ServiceProcess;
+
+using WAVE.lib.AnomalyEngine.ScriptTags.Base;
 
 namespace WAVE.lib.AnomalyEngine.ScriptTags
 {
+    [SupportedOSPlatform("windows")]
     public class CheckServiceTag : BaseScriptTags
     {
         protected override string[] ValidArguments => new[] { "servicename" };
 
         public override string Run(string[] arguments)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                using var sv = new ServiceController(arguments[1]);
+
+                if (sv.Status == ServiceControllerStatus.Running)
+                {
+                    return string.Empty;
+                }
+
+                return $"Service is not running, it's status is {sv.Status}";
+            }
+            catch (Exception ex)
+            {
+                return $"Exception occurred: {ex}";
+            }
         }
     }
 }
