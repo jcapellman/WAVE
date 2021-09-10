48b22f0f92849db91a1c73845bd844e24c7ee81e
Jarred Capellman <jcapellman@hotmail.com>
2021-09-10 08:07:07 -0500

    Fix: Resolved x86 hive redirection #8

--- src/WAVE.lib/PlatformImplementations/Windows/WindowsApplicationCheck.cs
+++ src/WAVE.lib/PlatformImplementations/Windows/WindowsApplicationCheck.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Runtime.Versioning;
 using System.Threading.Tasks;
 
 using Microsoft.Extensions.Logging;
@@ -50,103 +49,115 @@ namespace WAVE.lib.PlatformImplementations.Windows
             return string.IsNullOrEmpty(majorVersion) || string.IsNullOrEmpty(minorVersion) ? version : $"{majorVersion}.{minorVersion}";
         }
 
-        private List<ApplicationResponseItem> GetApps(string registryKeyPath, string appendingSuffix)
+        private List<ApplicationResponseItem> GetApps(string registryKeyPath)
         {
             var results = new ConcurrentDictionary<string, ApplicationResponseItem>();
 
-            using (var key = Registry.LocalMachine.OpenSubKey(registryKeyPath)) {
+            var hives = new RegistryHive[] { 
+                RegistryHive.CurrentUser, 
+                RegistryHive.LocalMachine 
+            };
 
-                if (key == null)
-                {
-                    return results.Values.ToList();
-                }
+            var views = new RegistryView[] { 
+                RegistryView.Registry32, 
+                RegistryView.Registry64 
+            };
 
-                Parallel.ForEach(key.GetSubKeyNames(), subKeyName =>
+            foreach (var hive in hives)
+            {
+                foreach (var view in views)
                 {
+                    RegistryKey key = null,
+                            basekey = null;
+
                     try
                     {
-                        using (var subKey = key.OpenSubKey(subKeyName))
-                        {
-
-                            if (subKey == null)
-                            {
-                                return;
-                            }
-
-                            var displayNameValue = subKey.GetValue("DisplayName");
-
-                            if (displayNameValue == null || string.IsNullOrEmpty(displayNameValue.ToString())) {
-                                LogDebug($"{subKeyName} had an empty Name - ignoring");
-
-                                return;
-                            }
-
-                            var item = new ApplicationResponseItem
-                            {
-                                Name = $"{displayNameValue} - ({appendingSuffix})",
-                                Version = ParseVersion(subKey.GetValue("Version")?.ToString(),
-                                    subKey.GetValue("DisplayVersion")?.ToString(), subKey.GetValue("VersionMajor")?.ToString(),
-                                    subKey.GetValue("VersionMinor")?.ToString()),
-                                Vendor = subKey.GetValue("Publisher")?.ToString(),
-                                InstallLocation = subKey.GetValue("InstallLocation")?.ToString(),
-                                InstallDate = ParseDate(subKey.GetValue("InstallDate")?.ToString())
-                            };
-
-                            if (item.InstallDate == null && !string.IsNullOrEmpty(item.InstallLocation) && Directory.Exists(item.InstallLocation))
-                            {
-                                try
-                                {
-                                    var directoryInfo = new DirectoryInfo(item.InstallLocation);
-
-                                    item.InstallDate = directoryInfo?.CreationTime;
-
-                                    LogDebug($"Obtained install date from {item.InstallLocation} ({item.InstallDate ?? null})");
-                                }
-                                catch (Exception iex)
-                                {
-                                    LogError($"Error when getting the install date from ({item.InstallLocation}): {iex}");
-                                }
-                            }
-
-                            results.TryAdd(item.Name, item);
-                        }
+                        basekey = RegistryKey.OpenBaseKey(hive, view);
+                        key = basekey.OpenSubKey(registryKeyPath);
                     }
-                    catch (Exception ex)
+                    catch (Exception ex) {
+                        LogDebug($"Error when attempting to obtain base key and key: {ex}");
+
+                        continue; 
+                    }
+
+                    if (basekey == null || key == null)
                     {
-                        LogError($"Error when parsing {subKeyName}: {ex}");
-                    }
-                });
+                        LogDebug($"Could not obtain base key or key - skipping");
 
-                return results.Values.ToList();
+                        continue;
+                    }
+
+                    Parallel.ForEach(key.GetSubKeyNames(), subKeyName =>
+                    {
+                        try
+                        {
+                            using (var subKey = key.OpenSubKey(subKeyName))
+                            {
+                                if (subKey == null)
+                                {
+                                    return;
+                                }
+
+                                var displayNameValue = subKey.GetValue("DisplayName");
+
+                                if (displayNameValue == null || string.IsNullOrEmpty(displayNameValue.ToString()))
+                                {
+                                    LogDebug($"{subKeyName} had an empty Name - ignoring");
+
+                                    return;
+                                }
+
+                                var item = new ApplicationResponseItem
+                                {
+                                    Name = displayNameValue.ToString(),
+                                    Version = ParseVersion(subKey.GetValue("Version")?.ToString(),
+                                        subKey.GetValue("DisplayVersion")?.ToString(), subKey.GetValue("VersionMajor")?.ToString(),
+                                        subKey.GetValue("VersionMinor")?.ToString()),
+                                    Vendor = subKey.GetValue("Publisher")?.ToString(),
+                                    InstallLocation = subKey.GetValue("InstallLocation")?.ToString(),
+                                    InstallDate = ParseDate(subKey.GetValue("InstallDate")?.ToString())
+                                };
+
+                                if (item.InstallDate == null && !string.IsNullOrEmpty(item.InstallLocation) && Directory.Exists(item.InstallLocation))
+                                {
+                                    try
+                                    {
+                                        var directoryInfo = new DirectoryInfo(item.InstallLocation);
+
+                                        item.InstallDate = directoryInfo?.CreationTime;
+
+                                        LogDebug($"Obtained install date from {item.InstallLocation} ({item.InstallDate ?? null})");
+                                    }
+                                    catch (Exception iex)
+                                    {
+                                        LogError($"Error when getting the install date from ({item.InstallLocation}): {iex}");
+                                    }
+                                }
+
+                                results.TryAdd(item.Name, item);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            LogError($"Error when parsing {subKeyName}: {ex}");
+                        }
+                    });
+                }
             }
+
+            return results.Values.ToList();
         }
 
         public override List<ApplicationResponseItem> GetInstalledApplications()
         {
             const string apps = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
 
-            var results = GetApps(apps, "x64");
+            var results = GetApps(apps);
 
-            if (Environment.Is64BitOperatingSystem)
-            {
-                LogDebug("64bit Operating System Detected - querying 32 bit applications");
+            LogDebug($"{results.Count} Applications found");
 
-                const string thirtyTwoBitApps = @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall";
-
-                results.AddRange(GetApps(thirtyTwoBitApps, "x86"));
-            }
-
-            var filteredResults = new List<ApplicationResponseItem>();
-
-            foreach (var result in results.Where(result =>
-                !filteredResults.Any(a => a.Name == result.Name && a.InstallLocation == result.InstallLocation)))
-            {
-                filteredResults.Add(result);
-            }
-
-            LogDebug($"{filteredResults.Count} Applications found");
-
-            return filteredResults.OrderBy(a => a.Name).ToList();
+            return results.OrderBy(a => a.Name).ToList();
         }
     }
 }
